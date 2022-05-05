b6c4ecd170cda09287c4aef4e5720eea71cca4c9
Marat Idrisov <idrisovm15@gmail.com>
2022-05-05 15:03:43 +0300

    SP-1137 Fix issues from audit

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Vesting {
     address public owner;
